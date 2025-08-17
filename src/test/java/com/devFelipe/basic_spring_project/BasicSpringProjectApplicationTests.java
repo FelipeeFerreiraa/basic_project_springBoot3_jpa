825b5aca5bb0a3c1ade74645c8a98c87bed80990
FelipeeFerreiraa <felipe333ferreira333@gmail.com>
2025-08-16 21:09:46 -0300

    Start project SpringBoot3_JPA --> 16/08/25 21:10

---
+++ src/test/java/com/devFelipe/basic_spring_project/BasicSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devFelipe.basic_spring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
